aa086af8ce38f306ef3cbcd94c3e2b79599db9ff
Jayyyyp <pjae9726@gmail.com>
2023-06-04 12:02:01 +0900

    yml blog

--- src/main/java/com/spring/blog/service/BlogService.java
+++ src/main/java/com/spring/blog/service/BlogService.java
@@ -1,7 +1,11 @@
 package com.spring.blog.service;
 
+import com.spring.blog.entity.Blog;
+
+import java.util.List;
+
 public interface BlogService {
     // 비즈니스 로직을 담당할 메서드를 "정의"만 하면 된다.
     // 전체 블로그 포스팅을 조회하는 메서드 findAll()을 선언하기
-
+    List<Blog> findAll();
 }
---
+++ src/main/java/com/spring/blog/service/BlogServiceImpl.java
@@ -0,0 +1,15 @@
+package com.spring.blog.service;
+
+import com.spring.blog.entity.Blog;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BlogServiceImpl implements BlogService{
+
+    @Override
+    public List<Blog> findAll() {
+        return null;
+    }
+}
